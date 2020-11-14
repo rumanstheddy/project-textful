42620e266635056e7b719d2bc9357348a92be4cf
Sum-RED <Sum-RED@.noreply.github.com>
2020-11-13 19:34:00 -0500

    added styling to admin page

--- textful/src/components/admin/AdminHome.js
+++ textful/src/components/admin/AdminHome.js
@@ -58,24 +58,70 @@ export default class AdminHome extends React.Component {
 
   renderUserList = () => {
     return this.state.userList.map((user) => (
-      <li class="list-group-item" onClick={() => this.goToProfile(user)}>
-        {user.userName}
-      </li>
+      <div class="d-flex flex-row border rounded">
+        <div class="pl-3 pt-2 pr-2 pb-2 w-75 border-left">
+          <h4 class="text-primary">{user.userName}</h4>
+          <p class="text-right m-0">
+            <button
+              class="btn btn-primary"
+              onClick={() => this.goToProfile(user)}
+            >
+              <i class="far fa-user"></i> View Profile
+            </button>
+          </p>
+        </div>
+      </div>
     ));
   };
 
   render() {
     return (
-      <div>
-        <input
-          name="text"
-          type="text"
-          placeholder="Search"
-          onChange={this.handleChange}
-        />
-        <button onClick={this.startSearch}>Search</button>
-        <h3>List of usernames to search...</h3>
-        <ul class="list-group">{this.renderUserList()}</ul>
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8">
+          <form class="card card-sm">
+            <div class="card-body row no-gutters align-items-center">
+              <div class="col-auto">
+                <i class="fas fa-search h4 text-body"></i>
+              </div>
+              <div class="col">
+                <input
+                  class="form-control form-control-lg form-control-borderless"
+                  type="search"
+                  name="text"
+                  placeholder="Search"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div class="col-auto">
+                <button
+                  type="button"
+                  class="btn btn-lg btn-success"
+                  onClick={this.startSearch}
+                >
+                  <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-search"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </form>
+          <h3>List of usernames to search:</h3>
+          {this.renderUserList()}
+        </div>
       </div>
     );
   }
