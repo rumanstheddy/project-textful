1bb46fa79377aafd086f641fe9f27b67da94e595
Sum-RED <Sum-RED@.noreply.github.com>
2020-11-13 19:43:59 -0500

    styling changes to admin page

--- textful/src/components/admin/AdminHome.js
+++ textful/src/components/admin/AdminHome.js
@@ -60,13 +60,28 @@ export default class AdminHome extends React.Component {
     return this.state.userList.map((user) => (
       <div class="d-flex flex-row border rounded">
         <div class="pl-3 pt-2 pr-2 pb-2 w-75 border-left">
-          <h4 class="text-primary">{user.userName}</h4>
+          <p class="text-primary" id="username">
+            {user.userName}
+          </p>
           <p class="text-right m-0">
             <button
               class="btn btn-primary"
               onClick={() => this.goToProfile(user)}
             >
-              <i class="far fa-user"></i> View Profile
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                class="bi bi-person"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+                />
+              </svg>
+               Profile
             </button>
           </p>
         </div>
