04243299d5b716920111e066821e16d20b441b4b
Sum-RED <Sum-RED@.noreply.github.com>
2020-11-30 13:38:07 -0500

    implemented default chat window

--- textful/src/components/chat/conversationList.js
+++ textful/src/components/chat/conversationList.js
@@ -5,12 +5,15 @@ const ConversationList = (props) => (
     <div class="sidebar-heading">{props.fullName}</div>
     <div class="list-group list-group-flush">
       {console.log(props)}
-      {props.contactList.map((userName) => (
+      {/* {props.contactList.map((userName) => (
         <a href="#" class="list-group-item list-group-item-action bg-light">
           {userName}
         </a>
-      ))}
+      ))} */}
     </div>
+    <button type="button" class="btn btn-primary rounded-circle" id="newConvoBtn">
+      <i class="fas fa-comment-medical"></i>
+    </button>
   </div>
 );
 
--- textful/src/components/chat/conversationView.js
+++ textful/src/components/chat/conversationView.js
@@ -8,35 +8,29 @@ const ConversationView = () => (
         <i class="fas fa-chevron-left"></i>
       </button>
 
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
       <a class="navbar-brand" id="contactName" href="#">
         Another user
       </a>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Sign out <i class="fas fa-sign-out-alt"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#" id="signoutLink">
+            Sign out <i class="fas fa-sign-out-alt"></i>
+          </a>
+        </li>
+      </ul>
     </nav>
 
     <div class="container-fluid">
-      <h1 class="mt-4">Chat comes here</h1>
+      <h1 class="font-weight-normal" id="defaultConvoViewTxt1">
+        Welcome {}
+      </h1>
+      <h5 class="font-weight-normal" id="defaultConvoViewTxt2">
+        Click on any of the contact names listed on the left to start chatting!
+        <br />
+        You can create a new conversation by clicking on the{" "}
+        <span id="newConvoInfoTxt">new conversation button</span> located on the bottom left.
+      </h5>
     </div>
   </div>
 );
