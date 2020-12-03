02a9d4005f847a04a6c86f4da58978dd733f038d
Durga Chillakuru <chillakuru.d@husky.neu.edu>
2020-12-03 12:13:59 -0500

    adding files for chat socket and session handling

---
+++ textful/src/services/ChatSocket.js
@@ -0,0 +1,19 @@
+import io from "socket.io-client";
+import * as sessionMgmt from './SessionHandler';
+
+var chatSocket = null;
+if (chatSocket === null)
+        chatSocket = io('https://wbdv-textful-server.herokuapp.com/');
+
+export function joinChat() {
+    chatSocket.emit('JOINING', sessionMgmt.getUserName())
+}
+
+export function registerForEvent(eventKey, callback) {
+
+    chatSocket.on(eventKey, callback);
+}
+
+export function sendMessage(eventKey, data) {
+    chatSocket.emit(eventKey, data)
+}
