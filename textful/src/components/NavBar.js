7606a0e651b80447069c2eb43686eb07e8f98314
Sum-RED <Sum-RED@.noreply.github.com>
2020-12-04 23:52:16 -0500

    implemented navbar for admin home

---
+++ textful/src/components/NavBar.js
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import * as sessionMgmt from "../services/SessionHandler";
+import history from "../services/History";
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let self = this;
+    return (
+      <div>
+        <Navbar
+          className="justify-content-between"
+          expand="lg"
+          bg="dark"
+          variant="dark"
+        >
+          <Navbar.Brand
+            onClick={() => history.push("/profile/" + self.props.id)}
+          >
+            {this.props.userName}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Nav className="flex-row">
+            {this.props.showSearch ? (
+              <NavItem className="ml-5">
+                <Form inline>
+                  {sessionMgmt.getUserRole() === "Admin" ? (
+                    <Nav.Link
+                      onClick={() => {
+                        history.push("/admin/" + self.props.id);
+                      }}
+                    >
+                      Admin Home
+                    </Nav.Link>
+                  ) : null}
+
+                  <Nav.Link
+                    onClick={() => {
+                      sessionMgmt.logout(self.props.userName);
+                      history.push("/login");
+                    }}
+                  >
+                    Sign out <i class="fas fa-sign-out-alt"></i>
+                  </Nav.Link>
+                  {/* <FormControl
+                    ref={this.inputRef}
+                    type="text"
+                    placeholder="Search"
+                    className="mr-sm-2"
+                  />
+                  <Button
+                    className="btn btn-primary"
+                    variant="primary"
+                    onClick={() =>
+                      history.push("/search/" + this.inputRef.current.value)
+                    }
+                  >
+                    Search
+                  </Button> */}
+                </Form>
+              </NavItem>
+            ) : null}
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
