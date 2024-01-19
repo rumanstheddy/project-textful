9f5b3c3dcae345253c87921fbc6ba2705033faeb
rumanstheddy <sumanthcob@gmail.com>
2024-01-18 20:38:40 -0500

    created http server from app

--- Server/Server.js
+++ Server/Server.js
@@ -1,50 +1,62 @@
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://admin:password001@textful.qrltkfp.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+const uri = process.env.DB_URL;
 try {
-  mongoose.connect( uri, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-  console.log("connected"));
-} catch (error) { 
+  mongoose.connect(
+    uri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("connected")
+  );
+} catch (error) {
   console.log("could not connect");
 }
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
 });
 
-const userController = require('./src/controllers/user.controller')
-const privateChatController = require('./src/controllers/privateChat.controller');
+const userController = require("./src/controllers/user.controller");
+const privateChatController = require("./src/controllers/privateChat.controller");
 const conversationController = require("./src/controllers/conversation.controller");
 const groupController = require("./src/controllers/groupChat.controller");
 const messageController = require("./src/controllers/message.controller");
 
-server = app.listen(process.env.PORT || 4000)
-const socket = require('socket.io')
-io = socket(server)
+const server = require("http").createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-let userNameToSocketId = {}
+const port = process.env.PORT || 4000;
 
-io.on('connection', (socket) => {
-    socket.on("JOINING", (data) => {
-        console.log("socket joining");
-        userNameToSocketId[data] = socket.id
-        conversationController(app, io, userNameToSocketId);
-    })
+server.listen(port, () => {
+  console.log("Listening on port", port);
+});
 
-    userController(app, io);
-    messageController(app, io);
-    privateChatController(app, io);
-    groupController(app, io);
-})
+let userNameToSocketId = {};
+
+io.on("connection", (socket) => {
+  socket.on("JOINING", (data) => {
+    console.log("socket joining");
+    userNameToSocketId[data] = socket.id;
+    conversationController(app, io, userNameToSocketId);
+  });
+
+  userController(app, io);
+  messageController(app, io);
+  privateChatController(app, io);
+  groupController(app, io);
+});
